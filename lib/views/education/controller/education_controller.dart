f358459fdbc8a1728be1800425ae9bf9d76183ac
basselamr <basselkamel20@gmail.com>
2025-02-25 14:44:14 +0200

    s

--- lib/views/education/controller/education_controller.dart
+++ lib/views/education/controller/education_controller.dart
@@ -1,5 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class EducationController extends GetxController{
+  var isLoading = false.obs;
+
+  Future<bool> addEducation(
+     String date, String time, String notes) async {
+    isLoading.value = true;
+    try {
+      await FirebaseFirestore.instance
+          .collection("medicine")
+          .add({"date": date, "time": time, "notes": notes});
+      Get.snackbar("Success", "Medicine added successfully!");
+      return true;
+    } catch (e) {
+      Get.snackbar("Error", "Something went wrong: $e");
+      return false;
+    } finally {
+      isLoading.value = false; // Reset loading state properly
+    }
+  }
   
 }
--- lib/views/medicine_notification/view/medicine_notification.dart
+++ lib/views/medicine_notification/view/medicine_notification.dart
@@ -5,11 +5,12 @@ import 'package:tamanina/views/medicine_notification/controller/medicine_control
 
 class MedicineNotifcationView extends StatelessWidget {
   MedicineNotifcationView({super.key});
+  
   final MedicineController _controller = Get.put(MedicineController());
 
   final dateController = TextEditingController();
   final timeController = TextEditingController();
-  final typeController = TextEditingController();
+  final nameController = TextEditingController();
   final notesController = TextEditingController();
 
   @override
@@ -17,133 +18,91 @@ class MedicineNotifcationView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-          child: Column(
-        spacing: 15,
-        children: [
-          SizedBox(height: 80,), Image.asset('assets/greenpill.png'),
-          Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20.w,
+        child: Column(spacing: 15,
+          children: [
+            SizedBox(height: 80),
+            Image.asset('assets/greenpill.png'),
+            
+            _buildTextField("الاسم", nameController),
+            _buildTextField("التاريخ", dateController),
+            _buildTextField("الوقت", timeController),
+            _buildTextField("الملحوظات", notesController, isMultiline: true),
+
+            SizedBox(height: 50),
+
+            Obx(() => GestureDetector(
+              onTap: () async {
+                bool success = await _controller.addMedicine(
+                  nameController.text, 
+                  dateController.text,
+                  timeController.text,
+                  notesController.text
+                );
+
+                if (success) {
+                  nameController.clear();
+                  dateController.clear();
+                  timeController.clear();
+                  notesController.clear();
+                }
+              },
+              child: Container(
+                width: 200,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color(0xffA8BDD2),
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(25),
                 ),
-            child: TextFormField(
-              controller: dateController,
-              keyboardType: TextInputType.text,
-              textDirection: TextDirection.rtl,
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.right,
-              decoration: InputDecoration(
-                  fillColor: Color(0xffF3F4F6),
-                  filled: true,
-                  hintText: "التاريخ",
-                  hintStyle: TextStyle(
-                    color: Colors.blueGrey,
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none)),
-            ),
-          ),
-          Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20.w,
-                ),
-            child: TextFormField(
-              controller: timeController,
-              keyboardType: TextInputType.text,
-              textDirection: TextDirection.rtl,
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.right,
-              decoration: InputDecoration(
-                  fillColor: Color(0xffF3F4F6),
-                  filled: true,
-                  hintText: "الوقت",
-                  hintStyle: TextStyle(
-                    color: Colors.blueGrey,
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none)),
-            ),
-          ),
-          Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20.w,
-                ),
-            child: TextFormField(
-              controller: typeController,
-              keyboardType: TextInputType.text,
-              textDirection: TextDirection.rtl,
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.right,
-              decoration: InputDecoration(
-                  fillColor: Color(0xffF3F4F6),
-                  filled: true,
-                  hintText: "النوع",
-                  hintStyle: TextStyle(
-                    color: Colors.blueGrey,
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none)),
-            ),
-          ),
-          Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20.w,
-                ),
-            child: TextFormField(minLines: 4,maxLines: 10,
-              controller: notesController,
-              keyboardType: TextInputType.text,
-              textDirection: TextDirection.rtl,
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.right,
-              decoration: InputDecoration(
-                  fillColor: Color(0xffF3F4F6),
-                  filled: true,
-                  hintText: "الملحوظات",
-                  hintStyle: TextStyle(
-                    color: Colors.blueGrey,
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none)),
-            ),
-          ),
-        SizedBox(height: 50,),
-        GestureDetector(
-                onTap: () {},
-                child: Container(
-                  width: 200,
-                  height: 60,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: const Color(0xffA8BDD2),
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(25)),
-                  child: Text(
-                    "حفظ",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25.sp,
-                      fontWeight: FontWeight.normal,
+                child: _controller.isLoading.value
+                  ? CircularProgressIndicator(
+                      color: Colors.white,
+                      backgroundColor: Colors.blueGrey,
+                      strokeWidth: 1.5,
+                    )
+                  : Text(
+                      "حفظ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.normal,
+                      ),
                     ),
-                  ),
-                ),
-              )
-        ],
-      )),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String hint, TextEditingController controller, {bool isMultiline = false}) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: TextInputType.text,
+        textDirection: TextDirection.rtl,
+        style: TextStyle(color: Colors.black),
+        textAlign: TextAlign.right,
+        minLines: isMultiline ? 4 : 1,
+        maxLines: isMultiline ? 10 : 1,
+        decoration: InputDecoration(
+          fillColor: Color(0xffF3F4F6),
+          filled: true,
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.blueGrey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none
+          ),
+        ),
+      ),
     );
   }
 }
